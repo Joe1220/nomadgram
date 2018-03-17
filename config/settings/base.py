399f99546d7685e460507e61da104e0c0dd32622
joe1220 <joe1220@daum.net>
2018-03-18 01:28:30 +0900

    postgres conn err fix

--- config/settings/base.py
+++ config/settings/base.py
@@ -39,7 +39,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
 DATABASES = {
-    'default': env.db('DATABASE_URL', default='postgres:///nomadgram'),
+    'default': env.db('DATABASE_URL', default='postgres://postgres:c159789c@localhost:5432/nomadgram'),
 }
 DATABASES['default']['ATOMIC_REQUESTS'] = True
 
