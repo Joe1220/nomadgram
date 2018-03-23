1316669a7f509db5a19f32c2713758f7a6a445d0
joe1220 <joe1220@daum.net>
2018-03-23 14:00:07 +0900

    delete a photo

--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -180,6 +180,13 @@ class ModerateComments(APIView):
 
 
 class ImageDetail(APIView):
+    
+    def find_own_image(self, image_id, user):
+        try:
+            image = models.Image.objects.get(id=image_id, creator=user)
+            return image
+        except models.Image.DoesNotExist:
+            return None
 
     def get(self, request, image_id, format=None):
 
@@ -196,9 +203,9 @@ class ImageDetail(APIView):
 
         user = request.user
 
-        try:
-            image = models.Image.objects.get(id=image_id, creator=user)
-        except models.Image.DoesNotExist:
+        image = self.find_own_image(image_id, user)
+
+        if image is None:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
 
         serializer = serializers.ImageInputSerializer(image, data=request.data, partial=True)
@@ -210,5 +217,17 @@ class ImageDetail(APIView):
             return Response(data=serializer.data, status=status.HTTP_204_NO_CONTENT)
 
         else:
-            
             return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, image_id, format=None):
+        
+        user = request.user
+
+        image = self.find_own_image(image_id, user)
+
+        if image is None:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+        image.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
