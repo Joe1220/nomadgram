9511e3e679e8272451658f2835852a9c52fbc431
joe1220 <joe1220@daum.net>
2018-03-23 13:49:03 +0900

    Editing a Photo, Deliting a Photo, Updating the Profile

--- nomadgram/images/serializers.py
+++ nomadgram/images/serializers.py
@@ -59,3 +59,14 @@ class ImageSerializer(serializers.ModelSerializer):
             'creator',
             'created_at'
         )
+
+    
+class ImageInputSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = models.Image
+        fields = (
+            'file',
+            'location',
+            'caption',
+        )
