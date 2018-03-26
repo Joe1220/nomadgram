aafb7bb4aec9dfc6c0c8d7fc536bae361eb67708
joe1220 <joe1220@daum.net>
2018-03-26 16:38:32 +0900

    Debugging Redux like a Pro with Reactotron

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,8 +1,9 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { routerReducer, routerMiddleware } from 'react-router-redux';
-import users from 'redux/modules/users';
-import createHistory from 'history/createBrowserHistory';
+import { combineReducers, createStore, applyMiddleware } from "redux";
+import { routerReducer, routerMiddleware } from "react-router-redux";
+import createHistory from "history/createBrowserHistory";
+import Reactotron from "ReactotronConfig";
+import thunk from "redux-thunk";
+import users from "redux/modules/users";
 
 const env = process.env.NODE_ENV;
 
@@ -10,17 +11,26 @@ const history = createHistory();
 
 const middlewares = [thunk, routerMiddleware(history)];
 
-if(env === "development") {
-    const { logger } = require("redux-logger");
-    middlewares.push(logger);
+if (env === "development") {
+  const { logger } = require("redux-logger");
+  middlewares.push(logger);
 }
 
 const reducer = combineReducers({
-    users,
-    routing: routerReducer
-})
+  users,
+  routing: routerReducer
+});
 
-let store = initialState => createStore(reducer, applyMiddleware(...middlewares))
+let store;
+
+if (env === "development") {
+  store = initialState =>
+    Reactotron.createStore(reducer, applyMiddleware(...middlewares));
+} else {
+  store = initialState => createStore(reducer, applyMiddleware(...middlewares));
+}
+
+
 
 export { history };
 
