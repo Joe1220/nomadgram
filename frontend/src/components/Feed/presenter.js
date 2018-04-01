3a2894286592dbeeeea751b2219e618220b5146d
joe1220 <joe1220@daum.net>
2018-04-01 17:30:01 +0900

    Created the Feed Component

---
+++ frontend/src/components/Feed/presenter.js
@@ -0,0 +1,11 @@
+import React from "react";
+import propTypes from "prop-types";
+import styles from "./styles.scss";
+
+const Feed = (props, context) => "Feed";
+
+Feed.propTypes = {
+  loading: propTypes.bool.isRequired
+}
+
+export default Feed;
