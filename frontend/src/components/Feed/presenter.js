3837bdc50aa7454db907a5dc4bd6eb1be8b97e49
joe1220 <joe1220@daum.net>
2018-04-02 00:00:56 +0900

    Loading and LoadingFeed Components

--- frontend/src/components/Feed/presenter.js
+++ frontend/src/components/Feed/presenter.js
@@ -1,8 +1,19 @@
 import React from "react";
 import propTypes from "prop-types";
 import styles from "./styles.scss";
+import Loading from "components/Loading";
 
-const Feed = (props, context) => "Feed";
+const Feed = (props, context) => {
+  if(props.loading) {
+    return <LoadingFeed />
+  }
+}
+
+const LoadingFeed = props => (
+  <div className={styles.feed}>
+    <Loading />
+  </div>
+)
 
 Feed.propTypes = {
   loading: propTypes.bool.isRequired
