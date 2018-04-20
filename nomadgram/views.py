03264f2bde7e135db3c7478ebae3dcd00225db1a
joe1220 <joe1220@daum.net>
2018-04-21 02:27:56 +0900

    nomadgram/views.py some fix

--- nomadgram/views.py
+++ nomadgram/views.py
@@ -3,12 +3,17 @@ from django.http import HttpResponse
 from django.conf import settings
 import os
 
+
 class ReactAppView(View):
 
     def get(self, request):
         try:
             with open(os.path.join(str(settings.ROOT_DIR), 'frontend', 'build', 'index.html')) as file:
                 return HttpResponse(file.read())
-
         except:
-            return HttpResponse(status=501)
+            return HttpResponse(
+                """
+                index.html not found ! build your React app !!
+                """,
+                status=501,
+            )
