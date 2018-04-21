24202f7449b8a0ed61f4d7c4f5dd8bce065bd4d5
joe1220 <joe1220@daum.net>
2018-04-21 21:04:22 +0900

    create superuser

--- nomadgram/users/management/commands/ebuser.py
+++ nomadgram/users/management/commands/ebuser.py
@@ -6,4 +6,4 @@ class Command(BaseCommand):
   def handle(self, *args, **options):
 
     User = get_user_model()
-    User.objects.create_superuser('joe1220', 'joe1220@daum.net')
+    User.objects.create_superuser('joe1220', 'joe1220@daum.net', 'c159789c')
