577d81e107adfbd9d069e2c80161358c437814ed
joe1220 <joe1220@daum.net>
2018-04-21 21:08:58 +0900

    superuser fix

--- nomadgram/users/management/commands/ebuser.py
+++ nomadgram/users/management/commands/ebuser.py
@@ -1,9 +1,10 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 
+
 class Command(BaseCommand):
 
-  def handle(self, *args, **options):
+    def handle(self, *args, **options):
 
-    User = get_user_model()
-    User.objects.create_superuser('joe1220', 'joe1220@daum.net', 'c159789c')
+        User = get_user_model()
+        User.objects.create_superuser('admin', 'admin@example.com', 'pass')
