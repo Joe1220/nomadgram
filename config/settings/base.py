6e511c0c804d9a17fdf014bad35b21918b666ca0
joe1220 <joe1220@daum.net>
2018-08-20 14:31:56 +0900

    change EMAIL_BACKEND

--- config/settings/base.py
+++ config/settings/base.py
@@ -218,7 +218,8 @@ FIXTURE_DIRS = (
 # EMAIL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
-EMAIL_BACKEND = env('DJANGO_EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
+# EMAIL_BACKEND = env('DJANGO_EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # ADMIN
 # ------------------------------------------------------------------------------
